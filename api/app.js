64898f4c7dab51b51fa69746de387c672a44af17
Vindhya <vindhya.raviraj@gmail.com>
2018-07-21 14:33:58 -0400

    updated header to allows cors from localhost

--- api/app.js
+++ api/app.js
@@ -8,7 +8,8 @@ require('dotenv').config();
 app.set('view engine', 'ejs');
 
 app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', 'https://vindhya.github.io');
+	// res.setHeader('Access-Control-Allow-Origin', 'https://vindhya.github.io');
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
 	next();
 }); 
 
