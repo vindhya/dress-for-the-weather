c157920203b7f65b461207aff5451d2c17ec1538
Vindhya <vindhya.raviraj@gmail.com>
2018-07-21 18:31:03 -0400

    added request to bing maps api for queries

--- api/app.js
+++ api/app.js
@@ -15,19 +15,38 @@ app.use(function(req, res, next) {
 
 app.get('/', (req, res) => res.send('hello world!'));
 
-app.get('/:latitude/:longitude', (req, res) => {
-	const latitude = req.params.latitude;
-	const longitude = req.params.longitude;
-	const url = `https://api.darksky.net/forecast/${process.env.SECRET_KEY}/${latitude},${longitude}?units=ca&exclude=[minutely,daily]`;
-	
+app.get('/:query', (req, res) => {
+	const query = req.params.query;
+	const url = `http://dev.virtualearth.net/REST/v1/Locations/${query}?maxResults=1&key=${process.env.BINGMAPS_KEY}`;
+
 	request(url, (error, response, body) => {
-		console.log(`requested dark sky api for ${latitude}, ${longitude}`);
+		console.log(`requested bing maps api for ${query}`);
+
 		if (!error && response.statusCode == 200) {
 			const data = JSON.parse(body);
 			res.send(data);
 		} else {
 			console.error(error);
 		}
+	});
+
+});
+
+app.get('/:latitude/:longitude', (req, res) => {
+	const latitude = req.params.latitude;
+	const longitude = req.params.longitude;
+	const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/${latitude},${longitude}?units=ca&exclude=[minutely,daily]`;
+	
+	request(url, (error, response, body) => {
+		console.log(`requested dark sky api for ${latitude}, ${longitude}`);
+
+		if (!error && response.statusCode == 200) {
+			const data = JSON.parse(body);
+			res.send(data);
+		} else {
+			console.error(error);
+		}
+
 	});
 
 });
