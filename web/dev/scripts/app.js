41a9152af209429b15d0c70ca7c36ce4847ebe09
Vindhya <vindhya.raviraj@gmail.com>
2018-07-21 18:54:58 -0400

    added ability to query weather via inputted location text

--- web/dev/scripts/app.js
+++ web/dev/scripts/app.js
@@ -1,17 +1,39 @@
-const app = {};
-
 // grab user's location via "use my location" button
 // or take location text input from form
 // send location to backend for API
 
+const app = {};
+
+app.domain = 'http://localhost:3000';
+
 app.getWeather = async (lat, long) => {
 	try {
-		const response = await fetch(`http://localhost:3000/${lat}/${long}`);
-		const weather = await response.json();
+		const weatherResponse = await fetch(`${app.domain}/${lat}/${long}`);
+		const weather = await weatherResponse.json();
 
 		console.log('dark sky response', weather);
 		return weather;
-		
+
+	} catch(err) {
+		console.error(err);
+	}
+};
+
+app.getWeatherFromQuery = async query => {
+	console.log(query);
+	try {
+		const geocodeResponse = await fetch(`${app.domain}/${query}`);
+		const geocode = await geocodeResponse.json();
+
+		console.log('bing maps response', geocode);
+		if (geocode.resourceSets[0].estimatedTotal === 0) {
+			$('#message').text('your query sucks');
+		} else {
+			const lat = geocode.resourceSets[0].resources[0].point.coordinates[0];
+			const long =  geocode.resourceSets[0].resources[0].point.coordinates[1];
+			return app.getWeather(lat, long);
+		}
+
 	} catch(err) {
 		console.error(err);
 	}
@@ -24,6 +46,11 @@ app.init = () => {
 			console.log('position', position);
 			app.getWeather(position.coords.latitude, position.coords.longitude);
     });
+	});
+
+	$('button[type=submit]').on('click', function(e) {
+		e.preventDefault();
+		app.getWeatherFromQuery($('input[type=text]').val());
 	});
 
 };
